cf211380a3f5e53af0d2b6cca9dd9e98a11a66f7
HousePlus Group Factory <jackhouseplus@gmail.com>
2026-04-24 01:27:48 +0800

    Update storyblok.ts

--- lib/storyblok.ts
+++ lib/storyblok.ts
@@ -2,12 +2,14 @@ export async function storyblokApi() {
   return {
     get: async (url: string, options: any) => {
       try {
-        const token = process.env.STORYBLOK_API_TOKEN || '';
+        // 读取你在 Vercel 设置的正确环境变量
+        const token = process.env.NEXT_PUBLIC_STORYBLOK_TOKEN || '';
         const version = options.version || 'published';
         const starts_with = options.starts_with || '';
+        const per_page = options.per_page || 100;
 
         const res = await fetch(
-          `https://api.storyblok.com/v2/cdn/stories?token=${token}&version=${version}&starts_with=${starts_with}&per_page=${options.per_page}`,
+          `https://api.storyblok.com/v2/cdn/stories?token=${token}&version=${version}&starts_with=${starts_with}&per_page=${per_page}`,
           { next: { revalidate: 60 } }
         );
 
