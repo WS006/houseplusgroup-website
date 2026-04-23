2be5a83ccf083a46017c7ffef09f6c14977e4826
HousePlus Group Factory <jackhouseplus@gmail.com>
2026-04-24 01:07:33 +0800

    Update storyblok.ts

--- lib/storyblok.ts
+++ lib/storyblok.ts
@@ -1,7 +1,21 @@
-import { StoryblokClient } from '@storyblok/client';
+export async function storyblokApi() {
+  return {
+    get: async (url: string, options: any) => {
+      try {
+        const token = process.env.STORYBLOK_API_TOKEN || '';
+        const version = options.version || 'published';
+        const starts_with = options.starts_with || '';
 
-export const storyblokApi = async () => {
-  return new StoryblokClient({
-    accessToken: process.env.STORYBLOK_API_TOKEN || '',
-  });
-};
+        const res = await fetch(
+          `https://api.storyblok.com/v2/cdn/stories?token=${token}&version=${version}&starts_with=${starts_with}&per_page=${options.per_page}`,
+          { next: { revalidate: 60 } }
+        );
+
+        return await res.json();
+      } catch (err) {
+        console.error('Storyblok API error', err);
+        return { data: { stories: [] } };
+      }
+    },
+  };
+}
