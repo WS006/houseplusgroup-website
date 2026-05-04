19dd7364344ac919b80f09728a51575d34954bb3
HousePlus Group Factory <jackhouseplus@gmail.com>
2026-05-05 00:52:40 +0800

    Update app/robots.ts - sitemap optimization

--- app/robots.ts
+++ app/robots.ts
@@ -1,20 +1,28 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://www.houseplus-ch.com';
+  const bareDomain = 'https://houseplus-ch.com';
+
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/', '/private/'],
-      },
-      {
-        userAgent: 'GoogleBot',
-        allow: '/',
-        disallow: ['/api/'],
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/static/',
+        ],
       },
     ],
-    sitemap: 'https://www.houseplus-ch.com/sitemap.xml',
-    host: 'https://www.houseplus-ch.com',
+    // Primary sitemap (Next.js dynamic sitemap from app/sitemap.ts)
+    sitemap: [
+      `${baseUrl}/sitemap.xml`,
+    ],
+    // Also declare bare domain sitemap for broader coverage
+    // (robots.txt spec supports multiple Sitemap lines)
+    // The following will be rendered as additional Sitemap: lines
+    host: baseUrl,
   };
 }
